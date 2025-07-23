a23f9f42b8d27a018436a35c6b7a24f4357ad5a6
ngumcaleb <keghamngum@gmail.com>
2025-07-23 07:30:04 -0700

    My first commit

---
+++ func.js
@@ -0,0 +1,28 @@
+ <script>
+      // All javascript Function types
+      //simple function
+      function greet() {
+        console.log("Hello, World!");
+      }
+
+      // Function with parameters
+      function greetUser(name) {
+        console.log(`Hello, ${name}!`);
+      }
+      // Function returning a value
+      function add(a, b) {
+        return a + b;
+      }
+      // Function expression
+      const multiply = function (a, b) {
+        return a * b;
+      };
+
+      // Arrow function
+      const divide = (a, b) => {
+        if (b === 0) {
+          throw new Error("Cannot divide by zero");
+        }
+        return a / b;
+      };
+    </script>
---
+++ index.js
@@ -0,0 +1,16 @@
+//Github commands
+git add .
+
+//Git commit 
+git commit -m "Initial commit"
+
+//Git push
+git push origin main
+//Git push to a specific branch
+// git push origin branch-name
+
+//git remote add origin
+// git remote add origin
+
+
+
